f37eca401edf6bbfab16f2e23dcc2525b43cd167
Hoàng Duy <duyh211199@gmail.com>
2021-09-03 17:36:09 +0700

    frondend

---
+++ src/admin/apiAdmin.js
@@ -0,0 +1,167 @@
+
+export const createCategory = (userId, token, category) => {
+    return fetch(`http://localhost:8888/api/category/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(category)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const createBranch = (userId, token, branch) => {
+    return fetch(`http://localhost:8888/api/branch/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(branch)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const createProduct = (userId, token, product) => {
+    return fetch(`http://localhost:8888/api/product/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const getCategories = () => {
+    return fetch(`http://localhost:8888/api/categories`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getBranches = () => {
+    return fetch(`http://localhost:8888/api/branches`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const listOrders = (userId, token) => {
+    return fetch(`http://localhost:8888/api/order/list/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getStatusValues = (userId, token) => {
+    return fetch(`http://localhost:8888/api/order/status-values/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+    return fetch(`http://localhost:8888/api/order/${orderId}/status/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ status, orderId })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getProducts = () => {
+    return fetch(`http://localhost:8888/api/products?limit=undefined`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const deleteProduct = (productId, userId, token) => {
+    return fetch(`http://localhost:8888/api/product/${productId}/${userId}`, {
+        method: "DELETE",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getProduct = productId => {
+    return fetch(`http://localhost:8888/api/product/${productId}`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const updateProduct = (productId, userId, token, product) => {
+    return fetch(`http://localhost:8888/api/product/${productId}/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
---
+++ src/core/Menu.js
@@ -0,0 +1,97 @@
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
+import { itemTotal } from "./cartHelpers";
+import { Navbar, Nav, Form, FormControl, Button, NavDropdown } from "react-bootstrap";
+
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else {
+    return { color: "#ffffff" };
+  }
+};
+
+
+const Menu = ({ history }) => (
+    
+  <Navbar bg="dark" expand="lg" variant="dark" className="justify-content-between">
+    <Navbar.Brand style={isActive(history, "/")} href="/">Second Hand</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="mr-auto my-2 my-lg-0"
+        style={{ maxHeight: "100px" }}
+        navbarScroll
+      >
+        <Nav.Link style={isActive(history, "/shop")} href="/shop">
+          Shop
+        </Nav.Link>
+        
+        {isAuthenticated() && isAuthenticated().user.role === 0 && (
+          <Nav.Link
+            style={isActive(history, "/user/dashboard")}
+            href="/user/dashboard"
+          >
+            Dashboard
+          </Nav.Link>
+        )}
+
+        {isAuthenticated() && isAuthenticated().user.role === 1 && (
+          <Nav.Link
+            className="nav-link"
+            style={isActive(history, "/admin/dashboard")}
+            href="/admin/dashboard"
+          >
+            Dashboard
+          </Nav.Link>
+        )}
+      </Nav>
+      <Form className="d-flex" style={{paddingRight: 150}}>
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="mr-2"
+          aria-label="Search"
+          style={{width: 400}}
+        />
+        <Button variant="outline-success" style={{borderRadius: 5}}>Search</Button>
+      </Form>
+      
+      <Nav>
+      <Nav.Link style={isActive(history, "/cart")} href="/cart">
+          Cart{" "}
+          <sup>
+            <small className="cart-badge">{itemTotal()}</small>
+          </sup>
+        </Nav.Link>
+        {!isAuthenticated() && (
+          <Fragment>
+            <Nav.Link style={isActive(history, "/signin")} href="/signin">
+              Signin
+            </Nav.Link>
+
+            <Nav.Link style={isActive(history, "/signup")} href="/signup">
+              Signup
+            </Nav.Link>
+          </Fragment>
+        )}
+        {isAuthenticated() && (
+          <Nav.Link
+            className="nav-link"
+            style={{ color: "#ffffff" }}
+            onClick={() =>
+              signout(() => {
+                history.push("/");
+              })
+            }
+          >
+            Signout
+          </Nav.Link>
+        )}
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default withRouter(Menu);
